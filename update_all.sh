7dfe87ee128a82d1f063f87f736370bff2e1f411
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-12-14 12:37:09 +0200

    update script

---
+++ update_all.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+# Init repos that are in .gitmodules but not cloned yet
+echo ""
+echo "----------------------- reinit nonexisting ---------------------------"
+git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+    while read path_key path
+    do
+        url_key=$(echo $path_key | sed 's/\.path/.url/')
+        url=$(git config -f .gitmodules --get "$url_key")
+        if [ ! -d "$path" ]; then
+            git submodule add $url $path
+        fi
+    done
+
+# Pull updates
+echo ""
+echo "----------------------- pull updates ---------------------------"
+git submodule foreach git pull origin master
+
+# Make sure checkout is at tip of master
+echo ""
+echo "----------------------- checkout master ---------------------------"
+git submodule foreach git checkout master
